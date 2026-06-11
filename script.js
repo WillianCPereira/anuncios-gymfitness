79dd59ac2cdd30c7027fbc04465dd479a4220c1c
WillianCPereira <willianedicao@gmail.com>
2026-06-11 09:25:45 -0300

    Refactor CONFIG object and update image paths

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const CONFIG = {
 
     anuncios:[
 
-        "anuncio1.png",
+        "https://github.com/WillianCPereira/anuncios-gymfitness/blob/main/anuncio1.png",
         "anuncio2.jpeg",
         "anuncio3.png",
         "anuncio4.png",
