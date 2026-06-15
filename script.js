ef4262fb419f01520338994b728fade4d11e960d
WillianCPereira <willianedicao@gmail.com>
2026-06-15 12:49:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const CONFIG = {
     // MODIFIQUE AQUI! ANIVERSARIANTE DO DIA (OU PROMOÇÃO TEMPORÁRIA)
     temporario:{
         ativo:true,
-        arquivo:"aniversariante1gabriel.jpeg",
+        arquivo:"aniversariante1.jpeg",
         inicio:"2026-06-15T08:00:00",
         duracaoHoras:24,
         tempoNaTela:12000
